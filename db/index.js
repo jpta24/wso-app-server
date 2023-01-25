a3f3e1b957cd8524c2a1318d0d3fe37dcdc4121d
Jpta24 <jeanpierretorres@gmail.com>
2023-01-25 23:15:39 +0100

    Signup done

--- db/index.js
+++ db/index.js
@@ -8,6 +8,8 @@ const mongoose = require("mongoose");
 const MONGO_URI =
   process.env.MONGODB_URI /* || "mongodb://127.0.0.1:27017/SecuApp" */;
 
+mongoose.set("strictQuery", false);
+
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
